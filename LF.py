8aa60d771d0c39cb321235fea99105bbbea5bf83
Ornela Marioni <ornela@orne.iate.oac.uncor.edu>
2018-04-23 15:46:40 -0300

    corregimos el lf.py

--- LF.py
+++ LF.py
@@ -3,7 +3,8 @@ import aceleraciones as ac
 
 def DKD(eps,dt,nit,m,pos,vel,acel):
 
-    path = '/home/users/valeria/metodos_numericos/_data/DKD/'
+    #path = '/home/users/valeria/metodos_numericos/_data/DKD/'
+    path = '/home/ornela/metodos_numericos/_data/DKD'
     
     pos[:,0] = pos[:,0] + vel[:,0]*dt*0.5
     pos[:,1] = pos[:,1] + vel[:,1]*dt*0.5
@@ -11,9 +12,10 @@ def DKD(eps,dt,nit,m,pos,vel,acel):
 
     for i in np.arange(nit):
         
+        acel = ac.aceleraciones(eps,pos[:,0],pos[:,1],pos[:,2],m)
         vel += acel*dt
         pos += vel*dt*0.5
-        acel = ac.aceleraciones(eps,pos[:,0],pos[:,1],pos[:,2],m)
+        
         
         if i in np.arange(0,nit,10):
             
@@ -27,7 +29,8 @@ def DKD(eps,dt,nit,m,pos,vel,acel):
 
 def KDK(eps,dt,nit,m,pos,vel,acel):
     
-    path = '/home/users/valeria/metodos_numericos/_data/KDK/'
+    #path = '/home/users/valeria/metodos_numericos/_data/KDK/'
+    path = '/home/ornela/metodos_numericos/_data/KDK'
     
     vel[:,0] = vel[:,0] + acel[:,0]*dt*0.5
     vel[:,1] = vel[:,1] + acel[:,1]*dt*0.5
